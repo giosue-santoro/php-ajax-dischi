1574f96ef1e51bf0d698c8209a8202a746447881
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-03-03 15:47:51 +0100

    problema server

--- app/server.php
+++ app/server.php
@@ -1,5 +1,9 @@
 <?php
 
-// 2nd milestone:
-// farà il json encode del mio database. => require_once
-// ricordatevi dell'header (vedi le slide)
+ require_once __DIR__ . "/../database/database.php";
+
+ header('Content-Type: application/json');
+ 
+ echo json_encode($dischi);
+
+?>
--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-new Vue ({
+// Access-Control-Allow-Origin: app/server.php;
+var app = new Vue({
     el: '#app',
-    data: '',
-    methods: ''
-})
+
+    data:{
+      dischi: [],
+    },
+
+    mounted() {
+
+      let self = this;
+  
+      axios.get("http://localhost/php-ajax-dischi/app/server.php")
+      .then((re) => {
+        self.dischi = re.data;
+      });
+
+    },
+
+    methods:{
+
+    }
+
+});
+Vue.config.devtools = true;
